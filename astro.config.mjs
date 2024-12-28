e6a55359a9a2c9270f31266d64810d518697764e
charlie mangano <eva.mangano@epfl.ch>
2024-12-28 13:16:59 +0100

    feat: update astro config with deploy settings
    set `site` to `https://xzbfsf.github.io` in `astro.config.mjs` as the final deployed url.
    set `base` to `/www/` as the base path of the repo.

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://xzbfsf.github.io',
+  base: '/www/',  // repo name
   markdown: {
     shikiConfig: {
       theme: 'nord'
